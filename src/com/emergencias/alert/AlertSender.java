72dac64f44dd433e30eff7b30cc401410f710fe9
David Galante Toledano <galante3807@gmail.com>
2025-11-06 16:42:53 +0100

    Cambios para pasar valores a AlertSender

--- src/com/emergencias/alert/AlertSender.java
+++ src/com/emergencias/alert/AlertSender.java
@@ -4,7 +4,20 @@ import com.emergencias.model.EmergencyEvent;
 //ENVIO DE ALERTA Y ALMACENAMIENTO DE DATOS
 public class AlertSender {
 
+    private final String[] datosHerido;
+    private final String[] datosUsuario;
+    private final String gravedad;
 
-    public void sendAlert(EmergencyEvent event) {
-        System.out.println("Enviar evento a 112. COMPLETAR ESTE MÉTODO");}
+    public AlertSender(String[] datosHerido, String[] datosUsuario, String gravedad) {
+        this.datosHerido = datosHerido;
+        this.datosUsuario = datosUsuario;
+        this.gravedad=gravedad;
+    }
+    
+    public void sendAlert(EmergencyEvent event){
+
+            System.out.println("Enviar evento a 112. COMPLETAR ESTE MÉTODO");
+        
+    }
+    
 }
